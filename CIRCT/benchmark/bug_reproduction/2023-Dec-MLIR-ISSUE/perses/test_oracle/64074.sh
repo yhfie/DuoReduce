8a448df5d6833484d06eafd66c06c3ce984251ba
wjy99-c <wjy99-c@github.com>
2024-07-23 21:50:35 -0700

    update test oracle script

--- CIRCT/benchmark/bug_reproduction/2023-Dec-MLIR-ISSUE/perses/test_oracle/64074.sh
+++ CIRCT/benchmark/bug_reproduction/2023-Dec-MLIR-ISSUE/perses/test_oracle/64074.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Execute the command and capture the output
-output=$(~/circt/llvm/build/bin/mlir-opt -test-vector-unrolling-patterns=unroll-based-on-type /scratch/jiyuan/MLIR-s-benchmark/CIRCT/benchmark/bug_reproduction/2023-Dec-MLIR-ISSUE/perses/generic_mlir/64074.mlir 2>&1)
+# output=$(~/circt/llvm/build/bin/mlir-opt --expand-strided-metadata --finalize-memref-to-llvm --lower-affine --convert-func-to-llvm --convert-arith-to-llvm --reconcile-unrealized-casts -test-vector-unrolling-patterns=unroll-based-on-type 64074.mlir 2>&1)
+# The above output motivate DualDD! Reduction in compilation path can results in further reduction in code! Remove --reconcile-unrealized-casts can trigger the error, but keep it cannot. To make the above compilation path trigger the error, we need more code logic in the IR code
+output=$(~/circt/llvm/build/bin/mlir-opt --expand-strided-metadata --finalize-memref-to-llvm --lower-affine --convert-func-to-llvm --convert-arith-to-llvm -test-vector-unrolling-patterns=unroll-based-on-type 64074.mlir 2>&1)
 
 # Search for "Assertion" and "Failed" in the output
 if [[ $output == *"Assertion"* && $output == *"failed"* ]]; then
-  echo 0
+  exit 0
 else
-  echo 1
+  exit 1
 fi
